8b12b54ce10387cd40dedd1fca66f0227c2a810d
drystar <chris.drysdale12@gmail.com>
2019-10-24 00:14:00 +0000

    play

--- play.js
+++ play.js
@@ -1,8 +1,27 @@
 const connect = require('./client');
+
 /**
  * Establishes connection with the game server
  */
 
+const setupInput = function() {
+  stdin.on('data', handleUserInput);
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
 
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+
+};
 console.log('Connecting ...');
 connect();
+
+module.exports = {setupInput};
