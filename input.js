6965453667afc9cd1b8f484a30ce778450cb8651
drystar <chris.drysdale12@gmail.com>
2019-10-24 01:02:07 +0000

    client

--- input.js
+++ input.js
@@ -1,18 +1,31 @@
-const setupInput = function() {
-  stdin.on('data', handleUserInput);
-  
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
+  stdin.on('data', handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  return stdin;
-
 };
 
-const handleUserInput = function (data) {
+const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
-  }
-};
+  
+  } else if (data === 'w') {
+    connection.write("Move: up");
 
+  } else if (data === 'a') {
+    connection.write("Move: left");
+
+  } else if (data === 's') {
+    connection.write("Move: down");
+
+  } else if (data === 'd') {
+    connection.write("Move: right");
+
+  }
+
+};
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,27 +1,6 @@
-const connect = require('./client');
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-/**
- * Establishes connection with the game server
- */
-
-const setupInput = function() {
-  stdin.on('data', handleUserInput);
-  
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-
-};
 console.log('Connecting ...');
-connect();
+setupInput(connect());
 
-module.exports = {setupInput};
